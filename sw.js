2a73a71c36031de688d25052c5c2a472ec78a71b
Jestamang <thejestamang@gmail.com>
2026-06-04 20:59:44 -0400

    perf: compress merch images (incl Bundle); SW v363

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // UPDATE CACHE_VERSION DATE ON EVERY PUSH
 // ════════════════════════════════════════════════════════════════
 
-var CACHE_NAME = 'jestamang-v362';
+var CACHE_NAME = 'jestamang-v363';
 var BADGE      = '/assets/icons/icon-192.png';
 
 var PRECACHE_URLS = [
